7bc39c6ff84d85d401d4931a110e4b6aa406ad66
AbdallahSaeed0 <bodyahly17@gmail.com>
2024-02-27 17:08:54 +0200

    installing-app

--- lib/register.dart
+++ lib/register.dart
@@ -12,59 +12,95 @@ class _RegisterState extends State<Register> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: AppTitle,),
-      body: ListView(
-                padding: EdgeInsets.symmetric(horizontal: 20),
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 50),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text("Registeration page", style: TextStyle(fontSize: 40, fontWeight: FontWeight.w900),),
-                Text("You're now creating an account as", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),)
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10),
-            child: Column(
-              children: [
-                InputField(label: "Full Name",),
-                SizedBox(height: 10,),
-                InputField(label: "Email",),
-                SizedBox(height: 10),
-                InputField(label: "Password",),
-                SizedBox(height: 10),
-                InputField(label: "Confirm Password",),
-                SizedBox(height: 25),
-                MaterialButton(
-                  onPressed: () {}, 
-                  color: Color.fromARGB(255, 168, 185, 230), 
-                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)), 
-                  elevation: 0, 
-                  padding: EdgeInsets.symmetric(horizontal: 130, vertical: 15), 
-                  child: Text("Sign Up", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),),),
-                SizedBox(height: 20,),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text("Already have an account? ", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),),
-                    InkWell(
-                      onTap: () {Navigator.of(context).pushReplacementNamed("login");}, 
-                      child: Text("Log in", 
-                            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20, 
-                            decoration: TextDecoration.underline, 
-                            decorationColor: Color.fromRGBO(93, 125, 212, 1), 
-                            color: Color.fromRGBO(93, 125, 212, 1)), ))
-                  ],
-                )
-              ],
-            )
-          )
-        ]
-
+      appBar: AppBar(
+        title: AppTitle,
       ),
+      body: ListView(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          children: [
+            const Padding(
+              padding: EdgeInsets.symmetric(vertical: 50),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Registeration page",
+                    style: TextStyle(fontSize: 40, fontWeight: FontWeight.w900),
+                  ),
+                  Text(
+                    "You're now creating an account as",
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  )
+                ],
+              ),
+            ),
+            Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: Column(
+                  children: [
+                    const InputField(
+                      label: "Full Name",
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    const InputField(
+                      label: "Email",
+                    ),
+                    const SizedBox(height: 10),
+                    const InputField(
+                      label: "Password",
+                    ),
+                    const SizedBox(height: 10),
+                    const InputField(
+                      label: "Confirm Password",
+                    ),
+                    const SizedBox(height: 25),
+                    MaterialButton(
+                      onPressed: () {},
+                      color: const Color.fromARGB(255, 168, 185, 230),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30)),
+                      elevation: 0,
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 130, vertical: 15),
+                      child: const Text(
+                        "Sign Up",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 25),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text(
+                          "Already have an account? ",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 20),
+                        ),
+                        InkWell(
+                            onTap: () {
+                              Navigator.of(context)
+                                  .pushReplacementNamed("login");
+                            },
+                            child: const Text(
+                              "Log in",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 20,
+                                  decoration: TextDecoration.underline,
+                                  decorationColor:
+                                      Color.fromRGBO(93, 125, 212, 1),
+                                  color: Color.fromRGBO(93, 125, 212, 1)),
+                            ))
+                      ],
+                    )
+                  ],
+                ))
+          ]),
     );
   }
 }
